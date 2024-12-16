36d47b414244a77014385c94c01f8756664ee679
SandraJGyebnar <gyebnar_sandraj@yahoo.com>
2024-12-16 20:21:30 +0200

    small refactors

--- src/test/java/sharedData/Hooks.java
+++ src/test/java/sharedData/Hooks.java
@@ -1,5 +1,7 @@
 package sharedData;
 
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class Hooks extends SharedData {
@@ -14,12 +16,12 @@ public class Hooks extends SharedData {
        // LoggerUtillity.startTest(testClassName);
     }
 
-//    @AfterMethod
-//    public void clearEnvironment(ITestResult result) {
-//        if (!result.isSuccess()) {
-//           // LoggerUtillity.errorLog(result.getThrowable().getMessage());
-//        }
-//        quitDriver();
-//       // LoggerUtillity.finishTest(testClassName);
-//    }
+    @AfterMethod
+    public void clearEnvironment(ITestResult result) {
+        if (!result.isSuccess()) {
+           // LoggerUtillity.errorLog(result.getThrowable().getMessage());
+        }
+        quitDriver();
+       // LoggerUtillity.finishTest(testClassName);
+    }
 }
--- src/test/java/tests/JobSearchInDeutschlandTest.java
+++ src/test/java/tests/JobSearchInDeutschlandTest.java
@@ -7,19 +7,19 @@ import pages.HomePage;
 import pages.JobSearchPage;
 import sharedData.Hooks;
 
-public class JobSearchInDeutschlandTest extends Hooks {
-
-    @Test
-    public void metodaTest(){
-
-
-        HomePage homePage = new HomePage(getDriver());
-        homePage.clickStellangebotButton();
-
-        JobSearchPage jobSearchPage = new JobSearchPage(getDriver());
-        JobSearchInDeutschlandModel testData = new JobSearchInDeutschlandModel(DataConfig.JOBSEARCH_DATA);
-
-        jobSearchPage.jobsSucheInDeutschland(testData);
-
-    }
-}
+//public class JobSearchInDeutschlandTest extends Hooks {
+//
+//    @Test
+//    public void metodaTest(){
+//
+//
+//        HomePage homePage = new HomePage(getDriver());
+//        homePage.clickStellangebotButton();
+//
+//        JobSearchPage jobSearchPage = new JobSearchPage(getDriver());
+//        JobSearchInDeutschlandModel testData = new JobSearchInDeutschlandModel(DataConfig.JOBSEARCH_DATA);
+//
+//        jobSearchPage.jobsSucheInDeutschland(testData);
+//
+//    }
+//}
