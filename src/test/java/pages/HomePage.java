057bce7bfeefeaf72d8ad64d0366c643a5bd775f
SandraJGyebnar <gyebnar_sandraj@yahoo.com>
2025-01-31 09:34:41 +0200

    Aktualisierung

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.testng.Assert;
 
 public class HomePage extends BasePage{
 
--- src/test/java/pages/JobSearchPage.java
+++ src/test/java/pages/JobSearchPage.java
@@ -4,44 +4,42 @@ import loggerUtillity.LoggerUtillity;
 import lombok.Getter;
 import lombok.Setter;
 import modelObject.JobSearchInDeutschlandModel;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.testng.Assert;
 
 @Getter
 @Setter
 
 public class JobSearchPage extends BasePage{
 
-    @FindBy(id="tomselect-1-ts-control")
+    @FindBy(id="tomselect-1")
     private WebElement landElement;
     @FindBy(id="tomselect-1-opt-2")
     private WebElement deutschLandElement;
-    @FindBy(id="tomselect-31-opt-3")
-    private WebElement schweizLandElement;
-    @FindBy(id="tomselect-31-opt-4")
-    private WebElement norwegenLandElement;
-    @FindBy(id="tomselect-31-opt-5")
-    private WebElement swedenLandElement;
-    @FindBy(id="tomselect-31-opt-6")
-    private WebElement oesterreichLandElement;
-    @FindBy(id="tomselect-31-opt-7")
-    private WebElement frankreichLandElement;
-    @FindBy(id="tomselect-2-ts-control")
+//    @FindBy(id="tomselect-31-opt-3")
+//    private WebElement schweizLandElement;
+//    @FindBy(id="tomselect-31-opt-4")
+//    private WebElement norwegenLandElement;
+//    @FindBy(id="tomselect-31-opt-5")
+//    private WebElement swedenLandElement;
+//    @FindBy(id="tomselect-31-opt-6")
+//    private WebElement oesterreichLandElement;
+//    @FindBy(id="tomselect-31-opt-7")
+//    private WebElement frankreichLandElement;
+    @FindBy(id="tomselect-2")
     private WebElement stadtElement;
-    @FindBy(id="tomselect-20-opt-2")
+    @FindBy(id="tomselect-2-opt-2")
     private WebElement arnstadtStadtElement;
-    @FindBy(id="tomselect-32-opt-50")
-    private WebElement opfikonStadtElement;
-    @FindBy(id="tomselect-3-ts-control")
+//    @FindBy(id="tomselect-32-opt-50")
+//    private WebElement opfikonStadtElement;
+    @FindBy(id="tomselect-3")
     private WebElement taetigkeitsbereichElement;
-    @FindBy(id="tomselect-3-opt-8" )
+    @FindBy(id="tomselect-25-opt-8" )
     private WebElement financeElement;
-    @FindBy(id="tomselect-4-ts-control")
+    @FindBy(id="tomselect-4")
     private WebElement anstellungsverhaeltnisElement;
-    @FindBy (id="tomselect-5-ts-control")
+    @FindBy (id="tomselect-5")
     private WebElement gesellschaftElement;
 
 
@@ -54,26 +52,26 @@ public class JobSearchPage extends BasePage{
     public void jobsSucheInDeutschland(JobSearchInDeutschlandModel testData){
         elementsMethods.selectDropDownElement(landElement,testData.getLandElement());
         LoggerUtillity.infoLog("Land wurde ausgewaehlt");
-        Assert.assertEquals(landElement, testData.getLandElement(), "Land selection mismatch!");
+//        Assert.assertEquals(landElement, testData.getLandElement(), "Deutschland");
 
 
         elementsMethods.selectDropDownElement(stadtElement, testData.getStadtElement());
-        LoggerUtillity.infoLog("Stand wurde ausgewaehlt");
-        Assert.assertEquals(stadtElement, testData.getStadtElement(), "Stadt selection mismatch!");
+        LoggerUtillity.infoLog("Stadt wurde ausgewaehlt");
+//        Assert.assertEquals(stadtElement, testData.getStadtElement(), "Arnstadt");
 
         elementsMethods.selectDropDownElement(taetigkeitsbereichElement, testData.getTaetigkeitsbereichElement());
         LoggerUtillity.infoLog("Taetigkeits Bereich wurde ausgewaehlt");
-        Assert.assertEquals(taetigkeitsbereichElement, testData.getTaetigkeitsbereichElement(), "Taetigkeitsbereich selection mismatch!");
+//        Assert.assertEquals(taetigkeitsbereichElement, testData.getTaetigkeitsbereichElement(), "Taetigkeitsbereich selection mismatch!");
 
 
         elementsMethods.selectDropDownElement(anstellungsverhaeltnisElement, testData.getAnstellungsverhaeltnisElement());
         LoggerUtillity.infoLog("Anstellungsverhaeltnis wurde ausgewaehlt");
-        Assert.assertEquals(anstellungsverhaeltnisElement, testData.getAnstellungsverhaeltnisElement(), "Anstellungsverhaeltnis selection mismatch!");
+//        Assert.assertEquals(anstellungsverhaeltnisElement, testData.getAnstellungsverhaeltnisElement(), "Anstellungsverhaeltnis selection mismatch!");
 
 
         elementsMethods.selectDropDownElement(gesellschaftElement, testData.getGesellschaftElement());
         LoggerUtillity.infoLog("Gesellschaft wurde ausgewaehlt");
-        Assert.assertEquals(gesellschaftElement, testData.getGesellschaftElement(), "Gesellschaft selection mismatch!");
+//        Assert.assertEquals(gesellschaftElement, testData.getGesellschaftElement(), "Gesellschaft selection mismatch!");
 
 
     }
